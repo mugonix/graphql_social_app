e88290ffaff1c04af590d997d0785be791edaa67
Tapiwanashe Mugoniwa <tmugonix@gmail.com>
2020-10-22 11:20:22 +0200

    Showing posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import  gql  from "graphql-tag";
+import { Grid, Transition } from 'semantic-ui-react';
+
+
+import PostCard from '../components/PostCard';
 
 function Home(props) {
-    const { loading, data: {getPosts: posts} } = useQuery(FETCH_POSTS_QUERY);
-    if(posts){
-        console.log(posts);
+    const {
+        loading,
+        data
+      } = useQuery(FETCH_POSTS_QUERY);
+    
+      if(data){
+        console.log(data);
     }
+
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+           
+          <Transition.Group>
+            {data.getPosts &&
+              data.getPosts.map((post) => (
+                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                  <PostCard post={post} />
+                </Grid.Column>
+              ))}
+          </Transition.Group>
+        )}
+      </Grid.Row>
+    </Grid>
     );
 }
 
